045dd14d0eb7c56e00ea76eaa54151f57fcecb76
alokgupta02 <alokik@zohomail.in>
2026-01-03 15:28:18 +0530

    commit

---
+++ src/Array/Array.java
@@ -0,0 +1,13 @@
+/**
+ * Author: Alok Gupta Date:03-01-2026 Time:15:10 Project: DSA
+ */
+
+package Array;
+
+public class Array {
+
+  public static void main(String[] args) {
+    System.out.println("hi");
+  }
+
+}
