01fed55c8c5afb57ac67dee0902bb8a0cd0a71db
alokgupta02 <alokik@zohomail.in>
2026-02-25 23:55:24 +0530

    Array manipulation ops

---
+++ src/Array/TwoPointer/MoveZeroesToEnd.java
@@ -0,0 +1,23 @@
+package Array.TwoPointer;
+
+public class MoveZeroesToEnd {
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 0, 3, 1, 2};
+        int n = arr.length;
+        int j = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != 0) {
+                arr[j] = arr[i];
+                j++;
+            }
+        }
+        for (int i = j; i < n; i++) {
+            arr[i] = 0;
+        }
+
+        for (int a : arr) {
+            System.out.print(a + " ");
+        }
+    }
+}
