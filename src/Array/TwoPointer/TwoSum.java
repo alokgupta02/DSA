f2a9ee048373324ec1b36e196b70100aea44aa5b
alokgupta02 <alokik@zohomail.in>
2026-03-05 05:19:57 +0530

    Array manipulation ops

--- src/Array/TwoPointer/TwoSum.java
+++ src/Array/TwoPointer/TwoSum.java
@@ -1,30 +1,64 @@
 package Array.TwoPointer;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class TwoSum {
 
     public static void main(String[] args) {
+        //array unsorted
         int[] arr = {2, 9, 4, 3, 8, 6, 7, 1, 6};
         int target = 12;
 
-        findSumIndex(arr, target);
-
+        // bruteForce(arr, target);
+        // better(arr, target);
+        optimal(arr, target);
     }
 
-    private static int[] findSumIndex(int[] arr, int target) {
+    private static int[] bruteForce(int[] arr, int target) {
 
-        int left = 0;
-        int right = left + 1;
         int n = arr.length;
 
         for (int i = 0; i < n; i++) {
-            int rem = target - arr[i];
             for (int j = i + 1; j < n; j++) {
-                if (arr[j] <= rem) {
-                    if (arr[j] == rem) {
-                        System.out.println(i+" "+j);
-                        return new int[]{i, j};
-                    }
+                if (arr[i] + arr[j] == target) {
+                    return new int[]{i, j};
                 }
+            }
+        }
+        return new int[]{};
+    }
+
+    private static int[] better(int[] arr, int target){
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i<arr.length; i++){
+            int rem = target - arr[i];
+            if(map.containsKey(rem)){
+                return new int[]{map.get(rem), i};
+            }
+            map.put(arr[i], i);
+        }
+        return new int[]{};
+    }
+
+    private static int[] optimal(int[] arr, int target){
+
+        Arrays.sort(arr);
+        int left = 0;
+        int right = arr.length-1;
+
+        while(left < right){
+            if(arr[left] + arr[right] > target){
+                right--;
+                continue;
+            }else if(arr[left] + arr[right] < target){
+                left++;
+                continue;
+            }
+            if(arr[left] + arr[right] == target){
+                return new int[]{arr[left],arr[right]};
             }
         }
         return new int[]{};
