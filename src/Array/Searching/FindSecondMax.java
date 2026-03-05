f26658f47bb6d1e01e1ec707629d2a474fd7b744
alokgupta02 <alokik@zohomail.in>
2026-03-05 06:06:14 +0530

    Array manipulation ops

--- src/Array/Searching/FindSecondMax.java
+++ src/Array/Searching/FindSecondMax.java
@@ -3,27 +3,25 @@ package Array.Searching;
 public class FindSecondMax {
 
     public static void main(String[] args) {
-        int[] arr = {5, 3, 8, 2, 9, 17, 16};
-        int max, secondMax;
-        int n = arr.length;
+        int[] arr = {10, 10, 10};
+        getSecondLargest(arr);
+    }
 
-        if(arr[0]>arr[1]){
-            max = arr[0];
-            secondMax = arr[1];
-        }else{
-            max = arr[1];
-            secondMax = arr[0];
-        }
+    private static int getSecondLargest(int[] arr) {
+        int first = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
 
-        for(int i=2; i<n;i++){
-            if(arr[i]>max){
-                secondMax = max;
-                max = arr[i];
-            }
-            else if(arr[i]>secondMax){
-                secondMax = arr[i];
+        for (int num : arr) {
+            if (num > first) {
+                second = first;
+                first = num;
+            } else if (num > second && num != first) {
+                second = num;
             }
         }
-        System.out.println(secondMax);
+        if (second == Integer.MIN_VALUE) {
+            return -1;
+        }
+        return second;
     }
 }
