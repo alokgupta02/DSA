0b0dc07ef573e373fb58b5ff023b6162af4c4911
Alok Gupta <alokik@zohomail.in>
2026-03-26 00:14:59 +0530

    Qs

---
+++ src/Array/SlidingWindow/MaxSumSubArray.java
@@ -0,0 +1,29 @@
+package Array.SlidingWindow;
+
+public class MaxSumSubArray {
+
+    public static void main(String[] args) {
+        int[] arr = { 100, 200, 300, 400, 500 };
+        int n = arr.length;
+        int k = 2;
+        int sum = 0, ans = 0;
+
+        // find first sum
+        for (int i = 0; i < k; i++) {
+            sum += arr[i];
+        }
+
+        int left = 0, right = k;
+
+        while (right < n) {
+            ans = Math.max(ans, sum);
+            sum += arr[right];
+            sum -= arr[left];
+            left++;
+            right++;
+        }
+        ans = Math.max(ans, sum);
+        System.out.println(ans);
+    }
+
+}
